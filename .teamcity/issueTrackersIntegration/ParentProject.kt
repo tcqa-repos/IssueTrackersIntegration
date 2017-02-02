ebcb6d27e264d151ecc897aec4f92467526c82d5
Julia Reshetnikova <lee.onore@gmail.com>
2017-02-02 18:24:57 +0300

    fix

--- .teamcity/issueTrackersIntegration/ParentProject.kt
+++ .teamcity/issueTrackersIntegration/ParentProject.kt
@@ -6,6 +6,7 @@ object ParentProject : Project({
     extId = "IssueTrackersIntegration"
     uuid = extId
     name = "Issue Trackers Integration Kotlin"
+    parentId = "CleanConfigurations_IssueTrackersIntegration"
 
     subProjects(issueTrackersIntegration.youtrack.Project)
     subProjects(issueTrackersIntegration.bugzilla.ParentProject)
