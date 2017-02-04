28553bcb16287300894f37760b691218ee08277c
Julia Reshetnikova <lee.onore@gmail.com>
2017-02-04 23:41:52 +0300

    update token

--- .teamcity/issueTrackersIntegration/ParentProject.kt
+++ .teamcity/issueTrackersIntegration/ParentProject.kt
@@ -44,7 +44,7 @@ object ParentProject : MyProject ("Issue Trackers Integration", {
         params {
             text("commit.message", "", display = ParameterDisplay.PROMPT, allowEmpty = false)
             param("file", "File.txt")
-            password("my.password", "zxxc60bd5a4f5bd34cb220d9a207b8d406a62c881aae03aa6ef8fcb560c3dbe1138516c0d0ee4d87467775d03cbe80d301b")
+            password("token", "c0f2e62dfb8ce0c157f6eb4405ce9b7590312c53")
             param("my.username", "tcqa-repos")
         }
 
@@ -52,7 +52,7 @@ object ParentProject : MyProject ("Issue Trackers Integration", {
             script {
                 scriptContent = """
                 echo {"message":"%commit.message%", "sha": "0d5a690c8fad5e605a6e8766295d9d459d65de42", "content": "YWFhCjExMQoyMjIK\n"} > data.txt
-                curl -X PUT -H "Authorization: token 3716d07a9b5cbf3a9b081e2f019ea4ab7600a44c" --data @data.txt https://api.github.com/repos/tcqa-repos/IssueTrackersIntegration/contents/File%build.counter%.txt
+                curl -X PUT -H "Authorization: token %token%" --data @data.txt https://api.github.com/repos/tcqa-repos/IssueTrackersIntegration/contents/File%build.counter%.txt
             """.trimIndent()
             }
         }
